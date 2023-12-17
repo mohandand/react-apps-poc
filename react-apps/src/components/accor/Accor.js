fd7022e69c17cfb13aa64d46ca22c1ca9821a954
mohan dandigam <mohandand@gmail.com>
2023-12-17 12:30:23 -0800

    Accordion complete

--- react-apps/src/components/accor/Accor.js
+++ react-apps/src/components/accor/Accor.js
@@ -3,16 +3,20 @@ import './Accor.css';
 
 export default function Accor({item, contetnt}){
     const [height, setHeight] = useState(0);
-
     const  contentRef = useRef(null);
+    function handleLogic(){
+        setHeight(( height === 0 ) ? contentRef.current.scrollHeight : 0)
+    }
     return(
         <>
-            <div className='item-name'>
+            <div className='item-name' onClick={handleLogic}>
                 {item}
             </div>
-            <div ref={contentRef} className='item-content' style={{maxHeight : '0px'}}>
+            <div ref={contentRef} className='item-content' style={{maxHeight : `${height}px`}}>
                 {contetnt}
             </div>
         </>
     )
 }
+
+//style={{maxHeight : '0px'}}
