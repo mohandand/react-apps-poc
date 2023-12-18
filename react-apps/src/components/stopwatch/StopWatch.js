c71bbb9a5e4a2013e939a3001a53311e81cf4725
mohan dandigam <mohandand@gmail.com>
2023-12-17 18:41:36 -0800

    stop watch

--- react-apps/src/components/stopwatch/StopWatch.js
+++ react-apps/src/components/stopwatch/StopWatch.js
@@ -1,15 +1,43 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 
 export default function StopWatch(){
-    const [time, settime] = useState(0);
+    const [time, setTime] = useState(0);
     const [start, setStart] = useState(false);
+    
+    useEffect(() => {
+        let interval;
+        if(start){
+            interval = setInterval(() => (setTime((prevTime) => {prevTime + 1})), 10);
+        }
+        if(!start){
+            clearInterval(interval)
+        }
+    }, [start])
 
+     // Hours calculation
+  const hours = Math.floor(time / 60*60*100);
+
+  // Minutes calculation
+  const minutes = Math.floor((time %  60*60*100) / 60 * 100);
+
+  // Seconds calculation
+  const seconds = Math.floor((time % 60* 100) / 100);
+
+  // Milliseconds calculation
+  const milliseconds = time % 100;
+    
     return(
         <div className='container'>
-            <span>00 :</span>
-            <span>00 :</span>
-            <span>00</span>
+         <div>
+            <span>{hours.toString().padStart(2,"0")}</span>
+            <span>{minutes.toString().padStart(2,"0")} :</span>
+            <span>{seconds.toString().padStart(2,"0")}</span>
+         </div>
+         <div className="buttons">
+            <button onClick={() => setStart(true)}>Start</button>
+            <button onClick={() => setStart(false)}>Stop</button>
+            <button onClick={() => setTime(0)}>Reset</button>       
+         </div>
         </div>
     )
-
 }
