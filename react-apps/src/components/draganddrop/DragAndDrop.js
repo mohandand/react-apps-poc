d9c371d6e85dad1ac222d166150c5daeeaa35bf9
mohan dandigam <mohandand@gmail.com>
2023-12-18 18:51:55 -0800

    Drag And Drop

--- react-apps/src/components/draganddrop/DragAndDrop.js
+++ react-apps/src/components/draganddrop/DragAndDrop.js
@@ -1,14 +1,32 @@
-import React, {useState} from 'react';
+import React, {useState, useRef} from 'react';
 import './draganddrop.css'
 export default function DragAndDrop(){
     const intilaData = ["Item1","Item2","Item3","Item4","Item5","Item6","Item7","Item8"];
     const [list, setList] = useState(intilaData);
+
+    const dragItem = useRef();
+    const dragOverItem = useRef();
+
+    function dragStart(e, dragItemIndex){
+        dragItem.current = dragItemIndex;
+    }
+
+    function dragEnter(e, dragOverItemIndex){
+        dragOverItem.current = dragOverItemIndex;
+    }
+
+    function drop(e){
+        const copyListItems = [...list];
+    }
     return(
         <div className="container">
             <h2>Drag And Drop</h2>
             {list.map(((item, index) => {
                 return(
-                    <div key={index} className='item-container'>
+                    <div key={index} className='item-container' draggable 
+                    onDragStart={(e) => {dragStart(e,index)}}
+                    onDragEnter={(e) => {dragEnter(e,index)}}
+                    onDragEnd={drop}>
                         {item}
                     </div>
                 )
