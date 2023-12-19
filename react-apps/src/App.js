e31155370f84bb8a02862b4c4ea4e4032c4bcb58
mohan dandigam <mohandand@gmail.com>
2023-12-18 17:08:11 -0800

    Table filter

--- react-apps/src/App.js
+++ react-apps/src/App.js
@@ -14,6 +14,7 @@ import CountDownTimer from './components/countdown/CountDownTimer';
 import StopWatch from './components/stopwatch/StopWatch';
 import AutoSuggsetion from './components/autosuggestion/AutoSuggestion';
 import ParentComponent from './components/usecontext/ParentComponent';
+import TableFilter from './components/tablefilter/TableFilter';
 const App = () => {
   return (
     <Router>
@@ -34,7 +35,7 @@ const App = () => {
               <Link className="link-button" to="/rating">Rating</Link>
             </button>
             <button>
-              <Link className="link-button" to="/tableoperations">TableOperations</Link>
+              <Link className="link-button" to="/tableoperations">TableSort</Link>
             </button>
             <button>
               <Link className="link-button" to="/infinitescroll">InfiniteScroll</Link>
@@ -60,6 +61,9 @@ const App = () => {
             <button>
               <Link className="link-button" to="/usecontext">UseContext</Link>
             </button>
+            <button>
+              <Link className="link-button" to="/tablefilter">TableFilter</Link>
+            </button>
         <hr />  
         <Routes>
         <Route path="/formvalidation" element={<FormValidation />} />
@@ -76,6 +80,7 @@ const App = () => {
           <Route path="/stopwatch" element={<StopWatch />} />
           <Route path="/autosuggestion" element={<AutoSuggsetion />} />
           <Route path="/usecontext" element={<ParentComponent />} />
+          <Route path="/tablefilter" element={<TableFilter />} />
         </Routes>
       </div>
     </Router>
--- react-apps/src/components/tablefilter/TableFilter.js
+++ react-apps/src/components/tablefilter/TableFilter.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {useState} from 'react';
+import './tablefilter.css';
 
 export default function TableFilter(){
     const intialData=  [
@@ -6,14 +7,43 @@ export default function TableFilter(){
         { id: 2, name: 'Jane', age: 30, volume: 40 },
         { id: 3, name: 'Doe', age: 22, volume: 35 },
       ];
+      const [tableData, setTableData] = useState(intialData);
+      const [searchValue, setSearchValue] = useState('');
+      const [selectedColumn, setSelectedColumn] = useState('');
+
+      function handleInputChnage(e){
+        setSearchValue(e.target.value);
+      }
+
+      function handleSelectValue(e){
+        setSelectedColumn(e.target.value);
+      }
+
+      function handleFilter(e){
+        e.preventDefault();
+        let filteredResult = intialData.filter((item) => {
+            return item[selectedColumn].toString().includes(searchValue.toString());
+        })
+        setTableData(filteredResult);
+      }
+
     return(
         <div className='container'>
             <h2>Table Filter</h2>
-            <div className="filter-area">
-
+            <div >
+            <form className="filter-area" onSubmit={handleFilter}>
+                <select onSelect={handleSelectValue}>
+                    <option>Id</option>
+                    <option>Name</option>
+                    <option>Age</option>
+                    <option>Volume</option>
+                </select>
+                <input type='text' placeholder='Enter value' onChange={handleInputChnage}/>
+                <button type="submit">Search</button>
+            </form>
             </div>
             <div className='table-area'>
-                <table>
+                <table className='table'>
                     <thead>
                         <tr>
                             <td>Id</td>
@@ -24,7 +54,7 @@ export default function TableFilter(){
                     </thead>
                     <tbody>
                         {
-                            intialData.map((row) => {
+                            tableData.map((row) => {
                                 return (
                                     <tr key={row.id}>
                                         <td>{row.id}</td>
